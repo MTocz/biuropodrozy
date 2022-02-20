f67ce6419fa6b21c97a5b66c588dbb7fdb7f272f
Martyna <martyna.toczyska@gmail.com>
2022-02-20 11:04:01 +0100

    Commit 1

--- biuropodrozy/Hotele.cs
+++ biuropodrozy/Hotele.cs
@@ -9,22 +9,20 @@ namespace biuropodrozy
     public class Hotele
     {
         
-        public Hotele(int kategoria, string nazwa, string kraj, string gwiazdki, int cena)
+        public Hotele(string nazwa, string kraj, string kategoria, int cena)
         {
             //ID = id;
-            Kategoria = kategoria;
             Nazwa = nazwa;
             Kraj = kraj;
-            Gwiazdki = gwiazdki;
+            Kategoria = kategoria;
             Cena = cena;
 
         }
 
         //public int ID { get; set; }
-        public int Kategoria { get; set; }
         public string Nazwa { get; set; }
         public string Kraj { get; set; }
-        public string Gwiazdki { get; set; }
+        public string Kategoria { get; set; }
         public int Cena { get; set; }
 
 
--- biuropodrozy/Program.cs
+++ biuropodrozy/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+Informatyka (Inż., sem. 1)
+Grupa nr 5​
+Michał Pokrywczyński (110902)​
+Martyna Toczyska (112785)​
+Róża Pietras (109228)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,17 +27,13 @@ namespace biuropodrozy
             //Wyświetlić napis promowane oferty
             Powitanie();
             //Wylosować jeden hotel z kategorii 1 i wyświetlić
-            //Kategoria ***
             Pokaz1();
             // Losowanie();
             //Wylosować jeden hotel z kategorii 2 z wyjątkiem miasta z losowania 1
             //Wylosować jeden hotel z kategorii 3 z wyjątkiem miasta z losowania 1 i 2
             //Zapytać o wybór hotelu
-            WyborOferty();
             //Zapytać o ilość osób dorosłych
-            PodajDoroslych();
             //Zapytać o ilość dzieci
-            PodajDzieci();
             //Pomnożyć cene z wybranego hotelu przez dorosłych i dzieci
             //Wyświetlić końcową cenę
         }
@@ -38,30 +42,25 @@ namespace biuropodrozy
         {
             _hotele = new List<Hotele>();
 
-            Hotele hotel = new Hotele(4, "Cancun Bay Resort", "Meksyk", "****", 450);
+            Hotele hotel = new Hotele("Cancun Bay Resort", "Meksyk", "****", 450);
             _hotele.Add(hotel);
-            _hotele.Add(new Hotele(5, "Iberostar Quetzal", "Meksyk", "*****", 690));
-            _hotele.Add(new Hotele(3, "Imperial Laguna by Faranda", "Meksyk", "***", 320));
-            _hotele.Add(new Hotele(5, "Playacalida", "Hiszpania", "*****", 600));
-            _hotele.Add(new Hotele(3, "Palia Puerto del Sol", "Hiszpania", "***", 240));
-            _hotele.Add(new Hotele(4, "Playa Real Resort", "Hiszpania", "****", 380));
-            _hotele.Add(new Hotele(3, "Sea Gull", "Egipt", "***", 270));
-            _hotele.Add(new Hotele(4, "Continental Hurghada", "Egipt", "****", 360));
-            _hotele.Add(new Hotele(5, "Sharm Grand Plaza", "Egipt", "*****", 620));
-            _hotele.Add(new Hotele(3, "Ikaros Hotel", "Grecja", "***", 220));
-            _hotele.Add(new Hotele(5, "Labranda Sandy Beach Resort", "Grecja", "*****", 580));
-            _hotele.Add(new Hotele(4, "Lida Corfu", "Grecja", "****", 310));
-            _hotele.Add(new Hotele(5, "Mytt Beach Hotel", "Tajlandia", "*****", 720));
-            _hotele.Add(new Hotele(4, "Novotel Rayong", "Tajlandia", "****", 410));
-            _hotele.Add(new Hotele(3, "Cholchan Pattaya Resort", "Tajlandia", "***", 290));
+            _hotele.Add(new Hotele("Iberostar Quetzal", "Meksyk", "*****", 690));
+            _hotele.Add(new Hotele("Imperial Laguna by Faranda", "Meksyk", "***", 320));
+            _hotele.Add(new Hotele("Playacalida", "Hiszpania", "*****", 600));
+            _hotele.Add(new Hotele("Palia Puerto del Sol", "Hiszpania", "***", 240));
+            _hotele.Add(new Hotele("Playa Real Resort", "Hiszpania", "****", 380));
+            _hotele.Add(new Hotele("Sea Gull", "Egipt", "***", 270));
+            _hotele.Add(new Hotele("Continental Hurghada", "Egipt", "****", 360));
+            _hotele.Add(new Hotele("Sharm Grand Plaza", "Egipt", "*****", 620));
+            _hotele.Add(new Hotele("Ikaros Hotel", "Grecja", "***", 220));
+            _hotele.Add(new Hotele("Labranda Sandy Beach Resort", "Grecja", "*****", 580));
+            _hotele.Add(new Hotele("Lida Corfu", "Grecja", "****", 310));
+            _hotele.Add(new Hotele("Mytt Beach Hotel", "Tajlandia", "*****", 720));
+            _hotele.Add(new Hotele("Novotel Rayong", "Tajlandia", "****", 410));
+            _hotele.Add(new Hotele("Cholchan Pattaya Resort", "Tajlandia", "***", 290));
+
 
         }
-
-        //void StworzKategorie()
-        //{
-        // KategorieHoteli = WszystkieHotele.Select(x => x.Kategoria).Distinct()
-        // .OrderBy(x => x).ToList();  
-        // }
 
 
         public static void Powitanie()
@@ -70,19 +69,6 @@ namespace biuropodrozy
             Console.WriteLine("---------------------------");
         }
 
-        //*public static void Grupowanie()
-        //{
-        // List<string> kat3 = new List<string>(Hotele);
-        //IEnumerable<IGrouping<string, string>> query = from gwi in gwiazdki
-        //group gwi by gwi = "***";
-        //foreach (var group in query)
-        // {
-        //Console.WriteLine(group.Key == 0);
-        //foreach (string i in group)
-        //Console.WriteLine(i);
-        //}
-
-        //}
 
         public static void Pokaz1()
         {
@@ -98,42 +84,35 @@ namespace biuropodrozy
                 Console.WriteLine("CENA: " + (hotel.Cena * 7) + " PLN/os");
                 Console.WriteLine("---------------------------");
 
-
+                if (numer > 3) break;
             }
-            
-        }
 
-        public static void WyborOferty()
-        {
-            Console.WriteLine("PROSZĘ PODAĆ NUMER WYBRANEJ OFERTY:");
-            string NumerOferty = Console.ReadLine();
-            int WybranaOferta = int.Parse(NumerOferty);
+                
+                Console.WriteLine("PROSZĘ PODAĆ NUMER WYBRANEJ OFERTY:");
+                string NumerOferty = Console.ReadLine();
+                int WybranaOferta = int.Parse(NumerOferty);
 
-            Hotele WybranyHotel = _hotele[WybranaOferta - 1];
-            Console.Clear();
+                Hotele WybranyHotel = _hotele[WybranaOferta - 1];
+                Console.Clear();
 
-        }
-        public static void PodajDoroslych()
+                
+                Console.WriteLine("PROSZĘ PODAĆ ILOŚĆ OSÓB DOROSŁYCH");
+                string dorosli = Console.ReadLine();
+                int IloscDoroslych = int.Parse(dorosli);
 
-        {
-            Console.WriteLine("PROSZĘ PODAĆ ILOŚĆ OSÓB DOROSŁYCH");
-            string dorosli = Console.ReadLine();
-            int IloscDoroslych = int.Parse(dorosli);
+                Console.Clear();
 
-            Console.Clear();
+                
+                Console.WriteLine("PROSZĘ PODAĆ ILOŚĆ DZIECI");
+                string dzieci = Console.ReadLine();
+                int IloscDzieci = int.Parse(dzieci);
 
-        }
-        public static void PodajDzieci()
-
-        {
-            Console.WriteLine("PROSZĘ PODAĆ ILOŚĆ DZIECI");
-            string dzieci = Console.ReadLine();
-            int IloscDzieci = int.Parse(dzieci);
-
-            Console.Clear();
-            Console.WriteLine("CAŁKOWITA CENA WAKACJI WYNOSI: ");
+                Console.Clear();
+                Console.WriteLine("CAŁKOWITA CENA WAKACJI WYNOSI: " + (
+                    WybranyHotel.Cena * IloscDoroslych + WybranyHotel.Cena * 0.5 * IloscDzieci
+                ) * 7 + (" PLN")
+                );
+            }
 
         }
-
     }
-}
